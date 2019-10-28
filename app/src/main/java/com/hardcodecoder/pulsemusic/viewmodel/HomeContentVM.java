96c89a1d7fd8f2a9edeb24013917c9d11c2ae15e
HardcodeCoder <hardcodecoder@gmail.com>
2019-10-28 16:01:53 +0530

    added new tracks list

--- app/src/main/java/com/hardcodecoder/pulsemusic/viewmodel/HomeContentVM.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/viewmodel/HomeContentVM.java
@@ -1,6 +1,7 @@
 package com.hardcodecoder.pulsemusic.viewmodel;
 
-
+import com.hardcodecoder.pulsemusic.model.AlbumModel;
+import com.hardcodecoder.pulsemusic.model.ArtistModel;
 import com.hardcodecoder.pulsemusic.model.MusicModel;
 import java.util.List;
 
@@ -8,8 +9,9 @@ public class HomeContentVM {
 
     private static final HomeContentVM singleInstance = new HomeContentVM();
     private List<MusicModel> mYourMayLikeList;
-    private List<MusicModel> mRecentArtist;
     private List<MusicModel> mNewInLibrary;
+    private List<AlbumModel> mAlbumsList;
+    private List<ArtistModel> mArtistList;
 
     public static HomeContentVM getInstance(){
         return singleInstance;
@@ -24,19 +26,27 @@ public class HomeContentVM {
         this.mYourMayLikeList = mYourMayLikeList;
     }
 
-    public List<MusicModel> getRecentArtist() {
-        return mRecentArtist;
-    }
-
-    public void setRecentArtist(List<MusicModel> mRecentArtist) {
-        this.mRecentArtist = mRecentArtist;
-    }
-
     public List<MusicModel> getNewInLibrary() {
         return mNewInLibrary;
     }
 
     public void setNewInLibrary(List<MusicModel> mNewInLibrary) {
         this.mNewInLibrary = mNewInLibrary;
+    }
+
+    public List<AlbumModel> getAlbumsList() {
+        return mAlbumsList;
+    }
+
+    public void setAlbumsList(List<AlbumModel> mAlbumsList) {
+        this.mAlbumsList = mAlbumsList;
+    }
+
+    public List<ArtistModel> getArtistList() {
+        return mArtistList;
+    }
+
+    public void setArtistList(List<ArtistModel> mArtistList) {
+        this.mArtistList = mArtistList;
     }
 }
